90b37119c356c1f7b1ae8a88acfb712a62f0953b
Alex Junior <alexjr2oficial@gmail.com>
2024-07-03 11:01:59 -0300

    update UpdateTask

--- ToDoApp.Web/Components/Pages/UpdateTaskComponents/UpdateTask.razor.cs
+++ ToDoApp.Web/Components/Pages/UpdateTaskComponents/UpdateTask.razor.cs
@@ -5,6 +5,16 @@ namespace ToDoApp.Web.Components.Pages.UpdateTaskComponents
 {
     public partial class UpdateTask
     {
-        
+        public List<TodoItem>? tasks;
+
+        private void ClickToUpdadate(int id)
+        {
+            Nav.NavigateTo($"/UpdateTask/taskid={id}");
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            tasks = await TodoService.GetTodosAsync();
+        }
     }
 }
